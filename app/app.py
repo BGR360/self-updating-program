9331252fec0115ba4b410083b3ab9826e219a6c9
Ben Reeves <benwolverine2019@gmail.com>
2019-10-19 21:26:08 -0400

    Add click dependency and use it to print version

--- app/app.py
+++ app/app.py
@@ -1,9 +1,5 @@
 from app.version import __version__
 
 class SelfUpdatingApp():
-    def print_version(self):
-        print('self-updating-app v{}'.format(__version__))
-
     def run(self):
-        self.print_version()
         print('Hello world!')
--- app/main.py
+++ app/main.py
@@ -1,5 +1,12 @@
+import click
+
 from app.app import SelfUpdatingApp
 
+# Define a command-line command using click
+@click.command()
+# Print version using --version (autodetected from setuptools)
+@click.version_option()
+@click.help_option('-h', '--help')
 def main():
     self_updating_app = SelfUpdatingApp()
     self_updating_app.run()
