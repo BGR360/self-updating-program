771ae11d41ec2f32aab3f0622d2fd720829235c2
Ben Reeves <benwolverine2019@gmail.com>
2019-10-19 21:04:06 -0400

    Detect version at runtime using PBR

--- app/app.py
+++ app/app.py
@@ -1,3 +1,9 @@
+from app.version import __version__
+
 class SelfUpdatingApp():
+    def print_version(self):
+        print('self-updating-app v{}'.format(__version__))
+
     def run(self):
+        self.print_version()
         print('Hello world!')
