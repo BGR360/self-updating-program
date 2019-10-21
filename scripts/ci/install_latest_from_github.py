6f84b8c36a7da16001aee654bb9799ad010f3ab1
Ben Reeves <benwolverine2019@gmail.com>
2019-10-21 19:26:36 -0400

    Add install_second_latest_and_update.py script

--- scripts/ci/install_latest_from_github.py
+++ scripts/ci/install_latest_from_github.py
@@ -5,11 +5,9 @@ If EXPECTED_VERSION is provided, will check that the version printed by
 self-updating-app --version matches.
 """
 
-import re
 import sys
-import subprocess
-from io import StringIO
-from subprocess import Popen, PIPE, STDOUT
+
+from common import subprocess_call_and_print, print_app_version
 
 GITHUB_REPO = sys.argv[1]
 EXPECTED_VERSION = None
@@ -18,34 +16,12 @@ if len(sys.argv) > 2:
 
 print('EXPECTED_VERSION = {}'.format(EXPECTED_VERSION))
 
-def subprocess_call_and_print(command):
-    print()
-    print(' '.join(command))
-    with Popen(command, stdout=PIPE, stderr=STDOUT, 
-               bufsize=1, universal_newlines=True) as p, StringIO() as output:
-        for line in p.stdout:
-            print(line, end='')
-            output.write(line)
-        stdout_contents = output.getvalue()
-        p.wait()
-        return_code = p.returncode
-    if return_code != 0:
-        print('exiting, returncode={}'.format(return_code))
-        sys.exit(return_code)
-    return stdout_contents
-
-
+# Install latest version from GitHub
 package = 'git+{}'.format(GITHUB_REPO)
 subprocess_call_and_print([sys.executable, '-m', 'pip', 'install', package])
 
-version_output = subprocess_call_and_print(['self-updating-app', '--version'])
-if EXPECTED_VERSION is not None:
-    # Extract version from output
-    match = re.search(r'version ([\d\.]+)', version_output)
-    if match:
-        version_string = match.group(1)
-        assert(version_string == EXPECTED_VERSION)
-        print('Version matches expected!')
+# Verify app version
+print_app_version(expected_version=EXPECTED_VERSION)
 
 subprocess_call_and_print(['self-updating-app'])
 
