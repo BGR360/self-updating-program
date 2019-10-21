425f9e5865ca5ffe998e769e0b37dba521402836
Ben Reeves <benwolverine2019@gmail.com>
2019-10-21 13:45:58 -0400

    More trying to fix Cirrus CI issues

--- app/main.py
+++ app/main.py
@@ -11,11 +11,13 @@ from app.app import SelfUpdatingApp
 # Allow the user to install a specific version with --install-version
 @click.option('--install-version',
               help='Install a specific version of the program (for testing).')
+@click.option('-n', '--no-update', is_flag=True,
+              help='Do not check for updates.')
 # Print version using --version (autodetected from setuptools)
 @click.version_option()
 @click.help_option('-h', '--help')
-def main(install_version):
-    self_updating_app = SelfUpdatingApp()
+def main(install_version, no_update):
+    self_updating_app = SelfUpdatingApp(no_update=no_update)
 
     if install_version:
         print('Installing version {}'.format(install_version))
